0234dc84194963f41eb6baa189a0fb2bbef55d5d
snyousef <34256558+snyousef@users.noreply.github.com>
2018-03-08 19:49:48 -0600

    Add files via upload

---
+++ TransactionData.R
@@ -0,0 +1,89 @@
+#Package for analyzing transactional data
+install.packages("arules")
+library(arules)
+require(arules) #loads all techniques within arules into environment
+??arules
+
+#Package that provides visual techniques for the arules package; required to install a few extra packages (TSP, caTools, whisker, and fpc) to properly run arulesViz
+install.packages("arulesViz", repos = "https://cran.r-project.org/web/packages/arulesViz/index.html", dependencies = TRUE)
+library(arulesViz)
+require(arulesViz)
+??arulesViz
+install.packages("TSP")
+library(TSP)
+install.packages("caTools")
+library(caTools)
+install.packages("whisker")
+library(whisker)
+install.packages("fpc")
+library(fpc)
+
+
+#Upload transaction comma-separated data
+Trans <- read.transactions("ElectronidexTransactions2017.csv", sep = ",") #Changes data into a sparse matrix (i.e.: each different product will have its own column and a '1' in the transaction row if the product was purchased (observation) and a '0' if it wasn't purchased)
+Trans #Returns all data from csv file; Question: Should I remove duplicate items? How do I remove items that meet certain requirements (i.e.: transactions with zero items purchased)
+
+summary(Trans)
+#Function above returns basic information about transaction data
+#Provides us with number of transactions per number of items purchased
+#Provides 5-number plot (min, max, median, etc.) for number of items purchased per transaction
+#Provides density (1 - sparsity)
+#Question: Why are there two transactions with zero items purchased
+9835*125*0.03506172 #43104 (out of total possible 9835*125 = 1229375) is the total number of items purchased among all 9,835 transactions
+43104/125 #Equals 344.832; if each product was purchased an equal number of times, it would be purchased 344 times
+344.832/43104 # Equals .008; Products with a support higher than .008 indicates a higher than average purchase rate
+
+
+inspect(Trans) #Veiw of all individual transactions
+inspect(Trans[1:5]) #Views first five transactions
+length(Trans) #Number of total transactions
+size(Trans) #Number of items per transaction
+LIST(Trans) #Lists the transactions from the dataset
+
+write.table(itemLabels(Trans), "ItemsforSale.txt") #Lists all the products sold and puts them into a text file
+itemLabels(Trans) #Lists all the products sold
+itemFrequency(Trans[,1]) #Prints support of item number listed (corresponds to the n-th item in dataset with n items); Support indicates percentage of times object appears out of total number of items purchased
+itemFrequency(Trans[,1:10]) #Prints support for each of first 10 items (columns); items appear alphabetically
+itemFrequencyPlot(Trans, support = .10) #Plots items that have >= support specified
+itemFrequencyPlot(Trans, topN = 5) #Returns items with the five highest support
+
+image(Trans[1:100]) #x-axis numbers correspond to each item sold; y-axis correspond to the transaction number; each dot corresponds to whether that item 'x' was purchased in transaction 'y'
+image(sample(Trans, 100)) #Does the same thing as above but takes a random sample of whatever amount specified
+
+
+
+#Confidence is a measure of the propotion of transactions where the presence of an item or set of items results in the presence of another set of items
+# conf{A,B} --> {C} = (support {A,B,C})/(support{A,B}); which transactions that have A and B (denominator) also have C (numerator)
+
+
+MBRule <- apriori(Trans, parameter = list(support=0.01, confidence=.50, minlen=2)) #Returns all transactions that meet or exceed support, confidence, and minlen parameters
+MBRule #Run to seee how many rules were created
+summary(MBRule) #Prints out general information about rules foundl; lhs means left-hand side, and rhs means right-hand side
+
+
+inspect(MBRule[1:19]) #Lift = confidence/support of right-hand side (rhs)
+#Lift is how much more likely an item is to be purchased relative to its general purchase rate (higher lift is better)
+#In printout results below, Apple Macbook Air is 2.48 times more likely to be purchased when a Dell KM117 Wireless Keyboard & Mouse are purchased than an Apple Macbook Air is to purchased among all transactions
+MBRuleInspect <- inspect(MBRule[1:19]) #Created object to create table (see about 20 lines below)
+
+
+inspect(sort(MBRule, by="support")[1:4]) # Sorts by 'by' parameter and returns first four rules; can sort by lift, support, count or confidence
+
+#Subset values based on subset rule
+help(subset)
+MBRuleSub <- subset(MBRule, subset = lhs %in% "HP Laptop") #Creates a subset of rules that meet subset= requirement
+MBRuleSub #Returns number of rules under specified subset rule
+inspect(MBRuleSub) #Prints only rules that meet information in subset = parameter
+
+
+is.redundant(MBRule) #Tests to see if any of the rules created are duplicates of each other
+is.redundant(MBRuleSub) #Tests to see if any of the rules created are duplicates of each other
+
+
+plot(MBRule, method="graph", control=list(type="p")) #Review https://www.rdocumentation.org/packages/graphics/versions/3.4.0/topics/plot
+plot(MBRule, method="graph", control=list(type="l")) #Review https://www.rdocumentation.org/packages/graphics/versions/3.4.0/topics/plot
+plot(MBRule, method="graph", control=list(type="b")) #Review https://www.rdocumentation.org/packages/graphics/versions/3.4.0/topics/plot
+plot(MBRule, method="graph", control=list(type="s")) #Review https://www.rdocumentation.org/packages/graphics/versions/3.4.0/topics/plot
+
+WriteTable <- write.table(MBRuleInspect, "MBRuleInspect.txt")
+write.csv(WriteTable, "MBRuleInspect.csv")
